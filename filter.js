6ee7edb6790894cf91a853357627d243946df0ac
Robn88 <robert.d.nicholas@gmail.com>
2022-02-01 14:34:31 +0000

    Finish module

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === 'Paul')
+console.log(paul)
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,27 @@ const students = [
     ]
   },
 ];
+
+const has5YeearsExp = skill => skill.yrsExperience >= 5;
+
+const hasStrongSkills = student => {
+  let strongSkills = student.skills.filter(has5YeearsExp);
+  return strongSkills.length > 0;
+}
+
+const candidates = students.filter(hasStrongSkills);
+
+console.log(candidates);
+
+const names = candidates.map(candidate => candidate.name);
+console.log(names)
+
+const namesAndJobs = candidates.map(candidate => [candidate.name, candidate.profession]);
+console.log(namesAndJobs);
+
+
+
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+
+// let evenNums = nums.filter(num => num % 2 ===0);
+// console.log(evenNums);
